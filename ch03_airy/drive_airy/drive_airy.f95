755e13c33b5495fc6851c2cc50c0c502de6c3785
Xue <yxue@mbs.tamu.edu>
2017-08-10 18:39:30 -0500

    done

--- ch03_airy/drive_airy/drive_airy.f95
+++ ch03_airy/drive_airy/drive_airy.f95
@@ -8,16 +8,16 @@ program abc
   complex(wp) :: xc, aic, daic
   complex(skind) :: xc1, aic1, daic1
   integer :: ierr, ierc, ierc1
-  ierc = 0
 
   call airy_ai(xr, air, dair, ierr)
   call airy_ai(xc, aic, daic, ierc)
-  call airy_ai(xc1, aic1, daic1, ierc)
+  call airy_ai(xc1, aic1, daic1, ierc1)
   xr = .5E0_wp
-  xc = (.50_wp)
+  xc = (.50_wp, .5E0_wp)
   xc1 = (.5E0_skind, .5E0_skind)
   print *, xr, air, dair
   print *, xc, aic, daic
   print *, xc1, aic1, daic1
-  print *, ierr, ierc
+  print *, ierr, ierc, ierc1
+  
 end program abc
